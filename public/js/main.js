3c948e84e883a0ff468745313de62897700b6a1b
AdilZd <nowayat0me@gmail.com>
2023-01-04 09:35:17 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+
+// 1 //
+
+// let adil = {
+//     nom : 'Zaidi',
+//     prenom : 'Adil',
+//     age : 22,
+//     taille : 1.8, 
+//     sePresenter(){
+//     console.log(`Bonjour je m'appelle ${adil.nom} ${adil.prenom}`);
+//     }
+// }
+// console.log(adil.sePresenter());
+
+// let lee = {
+//     nom : 'Jet',
+//     prenom : 'Lee',
+//     age : 42,
+//     taille : 1.7, 
+// }
+
+// let third = {
+//     nom : 'Zaidi',
+//     age : 42,
+//     taille : 1.9,
+// }
+
+// let changement = {
+//     nom : 'Change',
+//     prenom : 'Ment',
+//     age : 212,
+//     changerAge: function() {
+//         changement.age = prompt('Quel âge avez-vous maintenant ?');
+//     },
+//     afficherAge: function() {
+//         alert(changement.nom + ' a ' + changement.age + ' ans');
+//     }
+// }
+
+// changement.changerAge();
+// changement.afficherAge();
+
