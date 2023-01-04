abf7bdd373a237691235d305471c2e1bf4fe735b
AdilZd <nowayat0me@gmail.com>
2023-01-04 14:18:29 +0100

    fin

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,51 @@
+class Lieu {
+    constructor(nom) {
+        this.nom = nom;
+        this.contenu = [];
+    }
+}
+let maison = new Lieu("maison");
 
-let vieille_dame = {
-    age: 99,
-    nom: {
-        prenom: 'Laponie',
-        nomDeFamille: 'Cath'
-    },
-    moral: 'mal',
-    objet: 'canne',
-    parler(){
-        if (this.moral === 'mal') {
-            alert('Vous me dérangez et "le frappe avec une ' + this.objet + ' !"');
-        } else {
-            alert('Bonjour, ' + vieil_homme.prenom + '.');
-        }
+class Epicerie extends Lieu {
+    constructor(nom) {
+        super(nom);
+        this.ingredients = [];
+    }
+}
+let epicerie = new Epicerie("epicerie");
+
+class Cuisine extends Lieu {
+    constructor(nom) {
+        super(nom);
+        this.ingredients = [];
+    }
+}
+let cuisine = new Cuisine("cuisine");
+
+
+class Ingredients {
+    constructor(nom, etat, prix) {
+        this.nom = nom;
+        this.etat = etat;
+        this.prix = prix;
     }
 }
 
-let vieil_homme = {
-    nom: 'Jet',
-    adoucir () {
-        vieille_dame.moral = 'bien';
-    }
+let poivron = new Ingredient('poivron', 'entier', 1);
+let oignon = new Ingredient('oignon', 'entier', 2);
+let oeuf = new Ingredient('oeuf', 'entier', 4);
+let epice = new Ingredient('epice', 'moulu', 3.25);
+let paprika = new Ingredient('paprika', 'moulu', 1.5);
+let fromage = new Ingredient('fromage', 'coupé', 1.6);
+
+let personnage = {
+    nom: "Maxime",
+    lieu: "néant",
+    argent: 100,
+    panier: [],
+    se_deplacer(x) {},
+    payer(x) {},
+    couper(x, y) {}
 }
 
-vieille_dame.parler();
-vieil_homme.adoucir(vieille_dame);
-vieille_dame.parler();
+let personnage2 = new personnage();
